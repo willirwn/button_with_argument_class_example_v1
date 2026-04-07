f8c3b4c66f8363b5e7b3689cfc2ef7097ac91677
willirwn <willirwin100@gmail.com>
2026-04-07 13:43:39 -0400

    Add files via upload

---
+++ button_with_argument_class_example_v2/button_with_argument_class_example_v2.ino
@@ -0,0 +1,39 @@
+#include "BetterButton.h"
+
+int button1Pin = 31;
+int button2Pin = 32;
+int button3Pin = 33;
+int button4Pin = 34;
+
+BetterButton button1(button1Pin, 60);
+BetterButton button2(button2Pin, 62);
+BetterButton button3(button3Pin, 64);
+BetterButton button4(button4Pin, 65);
+
+void setup() {
+  button1.pressHandler(onPress);
+  button1.releaseHandler(onRelease);
+  button2.pressHandler(onPress);
+  button2.releaseHandler(onRelease);
+  button3.pressHandler(onPress);
+  button3.releaseHandler(onRelease);
+  button4.pressHandler(onPress);
+  button4.releaseHandler(onRelease);
+}
+
+void loop() {
+  button1.process();
+  button2.process();
+  button3.process();
+  button4.process();
+
+  usbMIDI.read();
+}
+
+void onPress(int note) {
+  usbMIDI.sendNoteOn(note, 99, 1);
+}
+
+void onRelease(int note) {
+  usbMIDI.sendNoteOff(note, 0, 1);
+}
